b00d881d887d6c21275e1aa234612287da60d626
Letao Zhang <zhangletao@agora.io>
2018-01-21 22:17:52 -0800

    add a script to run openVideoCall app

---
+++ OpenVideoCall/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./openVideoCall  --appId 111 \
+                 --uid 1024  \
+                 --channel demoChannel \
+                 --channelProfile 0 \
+                 --audioProfile 0 \
+                 --audioScenario 0 \
+                 --videoProfile 0 \
+                 --enableVideo 1 \
+                 --enableAudio 1 \
+                 --enableLocalVideo 1 \
+                 --enableLocalAudio 1 
